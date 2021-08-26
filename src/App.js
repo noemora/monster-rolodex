f846bafa1780fd0656277a29c92ca237a46f3717
Noe Mora <noe_cipo78@hotmail.com>
2021-08-26 17:52:35 -0300

    prueba push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
+
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -17,10 +20,21 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
